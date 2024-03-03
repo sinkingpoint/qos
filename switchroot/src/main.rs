b5b5028627e57e87596522ca47f42de7c4128c69
sinkingpoint <colin@quirl.co.nz>
2024-03-04 10:04:28 +1100

    Move switchroot logic into its own binary
    Rather than building that logic into qinit, move it into its own binary.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

---
+++ switchroot/src/main.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+
+use clap::{Arg, Command};
+use nix::mount::{mount, MsFlags};
+
+mod switchroot;
+
+fn create_device_folders() {
+	let device_folders = [
+		("/dev", "devtmpfs"),
+		("/proc", "proc"),
+		("/sys", "sysfs"),
+		("/run", "tmpfs"),
+		("/tmp", "tmpfs"),
+	];
+	for (folder, devtype) in device_folders {
+		std::fs::create_dir_all(folder).unwrap();
+		mount::<_, _, _, str>(Some(folder), folder, Some(devtype), MsFlags::empty(), None).unwrap();
+	}
+}
+
+fn main() {
+	let matches = Command::new("qinit")
+		.version("0.1.0")
+		.about("Switch the root filesystem")
+		.arg(
+			Arg::new("new_root")
+				.help("The new root filesystem")
+				.required(false)
+				.index(1),
+		)
+		.get_matches();
+
+	create_device_folders();
+	let new_root = matches.get_one::<PathBuf>("new_root").cloned();
+	let cmd = switchroot::SwitchrootCommand::new(new_root).unwrap();
+	cmd.run().unwrap();
+}
