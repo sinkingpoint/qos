d26676e946b4401bc45754e958237f42bdbd143a
sinkingpoint <colin@quirl.co.nz>
2024-03-04 21:49:53 +1100

    Execute `getty` from qinit
    In lieu of having a proper init system at the moment,
    we can just execute `getty` directly from qinit.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- qinit/src/main.rs
+++ qinit/src/main.rs
@@ -1,3 +1,9 @@
+use std::ffi::{CStr, CString};
+
+use nix::unistd::execve;
+
 fn main() {
-	println!("Hello, world!");
+	let command = CString::new("/sbin/getty").unwrap();
+	let args = [command.as_c_str(), &CString::new("/dev/tty1").unwrap()];
+	execve::<_, &CStr>(&command, &args, &[]).unwrap();
 }
