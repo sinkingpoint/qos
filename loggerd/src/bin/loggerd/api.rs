cc34998d45c3a8163c7bf4e9ed7b7caf3491cd0b
sinkingpoint <colin@quirl.co.nz>
2024-04-19 12:47:39 +1000

    Reorganise loggerd to have a separate api module
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

---
+++ loggerd/src/bin/loggerd/api.rs
@@ -0,0 +1,82 @@
+use std::path::{Path, PathBuf};
+
+use anyhow::Result;
+use loggerd::{LogMessage, OpenLogFile};
+use slog::error;
+use tokio::{
+	fs, io,
+	sync::{mpsc, Mutex},
+};
+
+pub struct Api {
+	logger: slog::Logger,
+	/// The pipe that the API receives logs over to write them to disk.
+	log_stream_read: Mutex<mpsc::Receiver<LogMessage>>,
+
+	/// The pipe that producers can write logs to.
+	log_stream_write: mpsc::Sender<LogMessage>,
+
+	data_dir: PathBuf,
+
+	log_files: Mutex<Vec<OpenLogFile>>,
+}
+
+impl Api {
+	pub fn new(data_dir: &Path, logger: slog::Logger) -> Self {
+		let (sender, receiver) = mpsc::channel(1024);
+		Self {
+			logger,
+			log_stream_read: Mutex::new(receiver),
+			log_stream_write: sender,
+			data_dir: data_dir.to_path_buf(),
+			log_files: Mutex::new(vec![]),
+		}
+	}
+
+	/// Load all the log files in the data directory.
+	async fn load_log_files(&self) -> io::Result<()> {
+		let mut log_files = fs::read_dir(&self.data_dir).await?;
+		while let Ok(Some(entry)) = log_files.next_entry().await {
+			let file_type = entry.file_type().await?;
+			if file_type.is_file() {
+				match OpenLogFile::open(&entry.path()).await {
+					Ok(file) => self.log_files.lock().await.push(file),
+					Err(e) => {
+						error!(self.logger, "Failed to open log file: {}", e);
+					}
+				}
+			}
+		}
+
+		self.log_files.lock().await.sort_by_key(|f| f.header.time_min);
+
+		Ok(())
+	}
+
+	pub async fn run(&self) -> Result<()> {
+		self.load_log_files().await?;
+		let mut log_files = self.log_files.lock().await;
+		let last_log_file = match log_files.last_mut() {
+			Some(file) => file,
+			None => {
+				let new_log_file = OpenLogFile::new(&new_random_log_file_name()).await?;
+				log_files.push(new_log_file);
+				log_files.last_mut().unwrap()
+			}
+		};
+
+		let mut log_stream = self.log_stream_read.lock().await;
+		loop {
+			let message = log_stream.recv().await.unwrap();
+			last_log_file.write_log(message).await?;
+		}
+	}
+
+	pub async fn write_log_stream(&self) -> mpsc::Sender<LogMessage> {
+		self.log_stream_write.clone()
+	}
+}
+
+fn new_random_log_file_name() -> PathBuf {
+	PathBuf::from(format!("log-{}.log", rand::random::<u64>()))
+}
--- loggerd/src/bin/loggerd/main.rs
+++ loggerd/src/bin/loggerd/main.rs
@@ -1,20 +1,17 @@
-use std::{
-	io::{self, stderr},
-	path::{Path, PathBuf},
-	sync::Arc,
-};
+mod api;
+
+use api::Api;
+use std::{io::stderr, path::PathBuf, sync::Arc};
 
 use anyhow::Result;
 use clap::{Arg, Command};
 use common::obs::assemble_logger;
 use control::listen::{Action, ActionFactory, ControlSocket};
-use loggerd::{control::START_WRITE_STREAM_ACTION, LogMessage, OpenLogFile};
+use loggerd::{control::START_WRITE_STREAM_ACTION, LogMessage};
 use slog::{error, info};
 use tokio::{
-	fs,
 	io::{AsyncBufReadExt, BufReader},
 	net::UnixStream,
-	sync::{mpsc, Mutex},
 };
 
 #[tokio::main]
@@ -142,73 +139,5 @@ impl WriteStreamHandler {
 
 	async fn run(self) -> Result<()> {
 		self.logstream().await
-	}
-}
-
-struct Api {
-	logger: slog::Logger,
-	/// The pipe that the API receives logs over to write them to disk.
-	log_stream_read: Mutex<mpsc::Receiver<LogMessage>>,
-
-	/// The pipe that producers can write logs to.
-	log_stream_write: mpsc::Sender<LogMessage>,
-
-	data_dir: PathBuf,
-
-	log_files: Mutex<Vec<OpenLogFile>>,
-}
-
-impl Api {
-	fn new(data_dir: &Path, logger: slog::Logger) -> Self {
-		let (sender, receiver) = mpsc::channel(1024);
-		Self {
-			logger,
-			log_stream_read: Mutex::new(receiver),
-			log_stream_write: sender,
-			data_dir: data_dir.to_path_buf(),
-			log_files: Mutex::new(vec![]),
-		}
-	}
-
-	/// Load all the log files in the data directory.
-	async fn load_log_files(&self) -> io::Result<()> {
-		let mut log_files = fs::read_dir(&self.data_dir).await?;
-		while let Ok(Some(entry)) = log_files.next_entry().await {
-			let file_type = entry.file_type().await?;
-			if file_type.is_file() {
-				match OpenLogFile::open(&entry.path()).await {
-					Ok(file) => self.log_files.lock().await.push(file),
-					Err(e) => {
-						error!(self.logger, "Failed to open log file: {}", e);
-					}
-				}
-			}
-		}
-
-		self.log_files.lock().await.sort_by_key(|f| f.header.time_min);
-
-		Ok(())
-	}
-
-	async fn run(&self) -> Result<()> {
-		self.load_log_files().await?;
-		let mut log_files = self.log_files.lock().await;
-		let last_log_file = match log_files.last_mut() {
-			Some(file) => file,
-			None => {
-				info!(self.logger, "No log files found");
-				return Ok(());
-			}
-		};
-
-		let mut log_stream = self.log_stream_read.lock().await;
-		loop {
-			let message = log_stream.recv().await.unwrap();
-			last_log_file.write_log(message).await?;
-		}
-	}
-
-	async fn write_log_stream(&self) -> mpsc::Sender<LogMessage> {
-		self.log_stream_write.clone()
 	}
 }
