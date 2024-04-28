3210cb2507e0ca7860865b89f5c1b89df861a10a
sinkingpoint <colin@quirl.co.nz>
2024-04-28 15:52:33 +1000

    Add ability to create users to auth crate
    This adds `create` methods to the `User` and `Group` structs in the
    auth crate. These methods allow for the creation of new users and
    groups on the system.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- auth/src/lib.rs
+++ auth/src/lib.rs
@@ -1,6 +1,13 @@
 mod sha;
+use chrono::DateTime;
 use sha::Sha2Mode;
-use std::{fs::read_to_string, io, path::PathBuf};
+use std::{
+	fmt::{self, Display, Formatter, Write},
+	fs::read_to_string,
+	io,
+	ops::Range,
+	path::PathBuf,
+};
 use thiserror::Error;
 
 /// The path to the passwd file.
@@ -11,6 +18,9 @@ const SHADOW_PATH: &str = "/etc/shadow";
 
 /// The path to the group file.
 const GROUP_PATH: &str = "/etc/group";
+
+/// The placeholder for a non-existent password (i.e an account that cannot be logged in).
+const NON_EXISTANT_PASSWORD: &str = "x";
 
 // Passwd file lines are in the format `<username>:<password>:<uid>:<gid>:<group>:<home>:<shell>`
 // Here we define the indices of each field in the colon separated passwd file line for easy access.
@@ -33,6 +43,9 @@ const SHELL_INDEX: usize = 6;
 /// The index in the colon separated shadow file line for the username.
 const SHADOW_PASSWORD_INDEX: usize = 1;
 
+/// The index in the colon separated shadow file line for the last changed field.
+const SHADOW_LAST_CHANGED_INDEX: usize = 2;
+
 /// A user on the system, that exists in the passwd file.
 pub struct User {
 	/// The username of the user.
@@ -52,6 +65,35 @@ pub struct User {
 }
 
 impl User {
+	pub fn create(
+		username: &str,
+		uid: Option<u32>,
+		gid: Option<u32>,
+		home: &str,
+		shell: &str,
+		password: Option<&str>,
+	) -> Result<Self, AuthError> {
+		let uid = match uid {
+			Some(uid) => uid,
+			None => next_uid(1000..65535)?.ok_or(AuthError::NoMoreIDs)?,
+		};
+
+		let gid = match gid {
+			Some(gid) => gid,
+			None => next_gid(1000..65535)?.ok_or(AuthError::NoMoreIDs)?,
+		};
+
+		ShadowEntry::create(username, password)?;
+		Group::create(username, Some(gid))?;
+		Ok(Self {
+			username: username.to_owned(),
+			uid,
+			gid,
+			home: PathBuf::from(home),
+			shell: PathBuf::from(shell),
+		})
+	}
+
 	pub fn get(selector: Selector) -> Result<Option<Self>, AuthError> {
 		match selector {
 			Selector::Name(name) => Self::from_username(&name),
@@ -121,10 +163,51 @@ pub struct ShadowEntry {
 	pub username: String,
 
 	/// The encrypted password of the user.
-	password_hash: HashedPassword,
+	password_hash: Option<HashedPassword>,
+
+	/// The last time the password was changed.
+	last_change: u32,
 }
 
 impl ShadowEntry {
+	fn create(username: &str, password: Option<&str>) -> Result<Self, AuthError> {
+		let password_hash = if let Some(password) = password {
+			Some(HashedPassword::from_crypt_password(password)?)
+		} else {
+			None
+		};
+
+		let new = Self {
+			username: username.to_owned(),
+			password_hash,
+			last_change: days_since_epoch(),
+		};
+
+		new.write()?;
+		Ok(new)
+	}
+
+	pub fn write(&self) -> Result<(), AuthError> {
+		let shadow = read_to_string(SHADOW_PATH)?;
+		let mut lines_to_write = Vec::new();
+		let mut exists = false;
+		for line in shadow.lines() {
+			match Self::from_shadow_line(line) {
+				Ok(entry) if entry.username == self.username => {
+					lines_to_write.push(self.to_string());
+					exists = true;
+				}
+				_ => lines_to_write.push(line.to_owned()),
+			}
+		}
+
+		if !exists {
+			lines_to_write.push(self.to_string());
+		}
+
+		Ok(())
+	}
+
 	fn from_shadow_line(line: &str) -> Result<Self, AuthError> {
 		let parts: Vec<&str> = line.split(':').collect();
 		if parts.len() != 9 {
@@ -135,11 +218,18 @@ impl ShadowEntry {
 			)));
 		}
 
-		let password = HashedPassword::from_crypt_password(parts[SHADOW_PASSWORD_INDEX])?;
+		let password = HashedPassword::from_crypt_password(parts[SHADOW_PASSWORD_INDEX])
+			.map(Some)
+			.unwrap_or(None);
+
+		let last_change = parts[SHADOW_LAST_CHANGED_INDEX].parse().map_err(|_| {
+			AuthError::Malformed(format!("malformed last changed: {}", parts[SHADOW_LAST_CHANGED_INDEX]))
+		})?;
 
 		Ok(Self {
 			username: parts[USERNAME_INDEX].to_string(),
 			password_hash: password,
+			last_change,
 		})
 	}
 
@@ -158,12 +248,36 @@ impl ShadowEntry {
 
 	/// Verifies the given password against the stored hash.
 	pub fn verify_password(&self, password: &str) -> Result<bool, AuthError> {
-		self.password_hash.verify(password)
+		match &self.password_hash {
+			Some(hashed) => hashed.verify(password),
+			None => Ok(false),
+		}
+	}
+}
+
+impl Display for ShadowEntry {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		let password = self
+			.password_hash
+			.as_ref()
+			.map(|p| p.to_string())
+			.unwrap_or(NON_EXISTANT_PASSWORD.to_string());
+
+		write!(f, "{}:{}:{}::::::", self.username, password, self.last_change)
 	}
 }
 
 enum PasswordAlgorithm {
 	Sha(Sha2Mode),
+}
+
+impl Display for PasswordAlgorithm {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		match self {
+			PasswordAlgorithm::Sha(Sha2Mode::Sha256) => f.write_char('5'),
+			PasswordAlgorithm::Sha(Sha2Mode::Sha512) => f.write_char('6'),
+		}
+	}
 }
 
 struct HashedPassword {
@@ -221,6 +335,12 @@ impl HashedPassword {
 	}
 }
 
+impl Display for HashedPassword {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		write!(f, "${}${}${}", self.algorithm, self.salt, self.hash)
+	}
+}
+
 /// A group on the system, that exists in the group file.
 pub struct Group {
 	/// The GID of the group.
@@ -231,6 +351,38 @@ pub struct Group {
 }
 
 impl Group {
+	pub fn create(name: &str, gid: Option<u32>) -> Result<Self, AuthError> {
+		let gid = gid.unwrap_or_else(|| next_gid(1000..65535).unwrap().unwrap());
+		let new = Self {
+			gid,
+			name: name.to_owned(),
+		};
+
+		new.write()?;
+		Ok(new)
+	}
+
+	fn write(&self) -> Result<(), AuthError> {
+		let group = read_to_string(GROUP_PATH)?;
+		let mut lines_to_write = Vec::new();
+		let mut exists = false;
+		for line in group.lines() {
+			let group = Self::from_group_line(line)?;
+			if group.gid == self.gid {
+				lines_to_write.push(format!("{}:{}:{}:", self.name, "x", self.gid));
+				exists = true;
+			} else {
+				lines_to_write.push(line.to_owned());
+			}
+		}
+
+		if !exists {
+			lines_to_write.push(format!("{}:{}:{}:", self.name, "x", self.gid));
+		}
+
+		Ok(())
+	}
+
 	pub fn get(selector: Selector) -> Result<Option<Self>, AuthError> {
 		match selector {
 			Selector::Name(name) => Self::from_groupname(&name),
@@ -305,6 +457,61 @@ pub enum AuthError {
 
 	#[error("Invalid password hash")]
 	AlwaysBad,
+
+	#[error("No more UIDs or GIDs available")]
+	NoMoreIDs,
+}
+
+fn days_since_epoch() -> u32 {
+	let now = chrono::Utc::now();
+	let then = DateTime::UNIX_EPOCH;
+	let duration = now.signed_duration_since(then);
+	duration.num_days() as u32
+}
+
+/// Returns the next available UID, or `None` if there are no more UIDs available.
+fn next_uid(acceptable_range: Range<u32>) -> Result<Option<u32>, AuthError> {
+	let mut uids = Vec::new();
+	let passwd = read_to_string(PASSWD_PATH)?;
+	for line in passwd.lines() {
+		let user = User::from_passwd_line(line)?;
+
+		if acceptable_range.contains(&user.uid) {
+			uids.push(user.uid);
+		}
+	}
+
+	uids.sort();
+
+	Ok(find_non_overlapping_value(acceptable_range, &uids))
+}
+
+/// Returns the next available GID, or `None` if there are no more GIDs available.
+fn next_gid(acceptable_range: Range<u32>) -> Result<Option<u32>, AuthError> {
+	let mut gids = Vec::new();
+	let group = read_to_string(GROUP_PATH)?;
+	for line in group.lines() {
+		let group = Group::from_group_line(line)?;
+
+		if acceptable_range.contains(&group.gid) {
+			gids.push(group.gid);
+		}
+	}
+
+	gids.sort();
+
+	Ok(find_non_overlapping_value(acceptable_range, &gids))
+}
+
+/// Returns the first value in the range that doesn't exist in the given values.
+fn find_non_overlapping_value(range: Range<u32>, values: &[u32]) -> Option<u32> {
+	for (i, value) in range.enumerate() {
+		if values[i] != value {
+			return Some(value);
+		}
+	}
+
+	None
 }
 
 #[cfg(test)]
@@ -328,10 +535,11 @@ mod test {
 			"test:$6$GkbfJlFNcqp8VGNn$9uWgXkCpoCCdoER/1yc1on8Rus0.eQHfLWkGth30liq9rL.joqL1hP/KfBXUHNT8fbwB44Txr1A01WoozxokQ/:19788:0:99999:7:::",
 		)
 		.unwrap();
+		let password = entry.password_hash.as_ref().unwrap();
 		assert_eq!(entry.username, "test");
-		assert_eq!(entry.password_hash.salt, "GkbfJlFNcqp8VGNn".to_owned());
+		assert_eq!(password.salt, "GkbfJlFNcqp8VGNn".to_owned());
 		assert_eq!(
-			entry.password_hash.hash,
+			password.hash,
 			"9uWgXkCpoCCdoER/1yc1on8Rus0.eQHfLWkGth30liq9rL.joqL1hP/KfBXUHNT8fbwB44Txr1A01WoozxokQ/".to_owned()
 		);
 
@@ -354,5 +562,14 @@ mod test {
 		assert_eq!(group.name, "root");
 
 		assert!(Group::from_group_line("YY").is_err());
+	}
+
+	#[test]
+	fn test_find_non_overlapping_value() {
+		let values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10];
+		assert_eq!(find_non_overlapping_value(0..10, &values), Some(9));
+
+		let values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+		assert_eq!(find_non_overlapping_value(0..10, &values), None);
 	}
 }
